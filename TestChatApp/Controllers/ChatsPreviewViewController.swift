3d9506dd4a91797c86fec101b2dbc2bd09a8803d
Anna Vorontsova <annagogley@gmail.com>
2021-03-25 12:19:20 +0300

    Core data

--- TestChatApp/Controllers/ChatsPreviewViewController.swift
+++ TestChatApp/Controllers/ChatsPreviewViewController.swift
@@ -12,17 +12,9 @@ class ChatsPreviewViewController: UIViewController {
     
     @IBOutlet weak var chatTableView: UITableView!
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-    var chatPreview : [ChatPreview]?
+    var chatPreview = [ChatPreview]()
+    var buttonSelection:Bool = false
     
-    var messages : [Message] =
-        [
-            Message(body: "Hello, neighbour!", time: "19:27"),
-            Message(body: "Somethibg long it's a big message way a as as ", time: "09:27"),
-            Message(body: "It's test chat", time: "13:06"),
-        ]
-    
-    
-
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -38,43 +30,81 @@ class ChatsPreviewViewController: UIViewController {
             
         }
         chatTableView.dataSource = self
-        fetchChats()
+        loadChats()
+        print(chatPreview.count)
     }
     
-    func fetchChats() {
+    override func viewDidAppear(_ animated: Bool) {
+        loadChats()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        saveChats()
+        if buttonSelection {
+            print("++++")
+        }
+    }
+    
+//MARK: - data manipulation
+    
+    func loadChats() {
         //fetch the data from Core Data to display in table view
+        let request : NSFetchRequest<ChatPreview> = ChatPreview.fetchRequest()
         do {
-            try context.fetch(ChatPreview.fetchRequest())
-            DispatchQueue.main.async {
-                self.chatTableView.reloadData()
-            }
+            chatPreview = try context.fetch(request)
         }
         catch {
             print("Couldn't fetch data due to error: \(error)")
         }
+        chatTableView.reloadData()
     }
     
+    func saveChats() {
+        do {
+            try context.save()
+        }
+        catch {
+            print("Couldn't save data due to error \(error)")
+        }
+        print("savingChats")
+        chatTableView.reloadData()
+    }
+    
+    
+    
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        buttonSelection = true
+        print(buttonSelection)
+        let newChat = ChatPreview(context: context)
+        //генерируем id
+        let uuid = UUID().uuidString
+        newChat.id = uuid
+        newChat.body = "Something"
+        newChat.time = "16:44"
+        chatPreview.append(newChat)
+        saveChats()
+        print(newChat)
+        print("add button pressed")
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0, execute: {
+            print("delayed")
+            self.performSegue(withIdentifier: "goToChat", sender: self)
+        })
     }
     
 
 }
 
-
-
 //MARK: - extension Data Source
 
 extension ChatsPreviewViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return messages.count
+        return chatPreview.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let message = messages[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! ChatPreviewTableViewCell
-        cell.chatPreviewLabel.text = message.body
-        cell.timeLabel.text = message.time
-//        cell.delegate = self
+        cell.chatPreviewLabel.text = chatPreview[indexPath.row].body!
+        cell.timeLabel.text = chatPreview[indexPath.row].time!
         
         return cell
     }
@@ -88,7 +118,10 @@ extension ChatsPreviewViewController: UITableViewDataSource, UITableViewDelegate
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let destinationVC = segue.destination as! ChatViewController
         
-        //set certain chat was selected 
+        //set certain chat was selected
+        if let indexPath = chatTableView.indexPathForSelectedRow {
+            destinationVC.selectedChat = chatPreview[indexPath.row]
+        }
     }
     
     func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
@@ -96,21 +129,13 @@ extension ChatsPreviewViewController: UITableViewDataSource, UITableViewDelegate
         let action = UIContextualAction(style: .destructive, title: "") { (action, view, completionHandler) in
             
             //which chat to remove
-            let chatToRemove = self.chatPreview![indexPath.row]
+            let chatToRemove = self.chatPreview[indexPath.row]
             
             //remove the chat
             self.context.delete(chatToRemove)
             
             //save the data
-            do {
-                try self.context.save()
-            }
-            catch {
-                print("Couldn't remove data due to: \(error)")
-            }
-            
-            //refecth the data
-            self.fetchChats()
+            self.saveChats()
         }
         action.backgroundColor = UIColor(patternImage: UIImage(named: K.deleteIcon)!)
         
--- TestChatApp/Controllers/StartScreenViewController.swift
+++ TestChatApp/Controllers/StartScreenViewController.swift
@@ -18,24 +18,45 @@ class StartScreenViewController: UIViewController {
         super.viewDidLoad()
         startScreenOutlet.dropshadow(color: UIColor.black, opacity: 0.5, radius: 4)
         enterButtonOutlet.dropshadow(color: UIColor(named: K.brandColors.redShadow)!, opacity: 0.5, radius: 9)
+        
         enterButtonOutlet.layer.backgroundColor = UIColor(named: K.brandColors.redColor)!.cgColor
         enterButtonOutlet.layer.cornerRadius = enterButtonOutlet.frame.height / 2
         enterButtonOutlet.titleLabel?.text = "Enter"
         enterButtonOutlet.titleLabel?.tintColor = .white
         enterButtonOutlet.titleLabel?.font = .systemFont(ofSize: 18, weight: .bold)
+        enterButtonOutlet.alpha = 0
+        
+        UIView.animate(withDuration: 1.0) {
+                self.enterButtonOutlet.alpha = 1.0
+            }
+        
+        
+        
+        UIView.animate(withDuration: 0.2, delay: 1.0,
+            animations: {
+                self.enterButtonOutlet.transform = CGAffineTransform(scaleX: 0.95, y: 0.95)
+            },
+            completion: { _ in
+                UIView.animate(withDuration: 0.2) {
+                    self.enterButtonOutlet.transform = CGAffineTransform.identity
+                }
+            })
     }
     
     
     
     override func viewWillAppear(_ animated: Bool) {
         navigationController?.navigationBar.isHidden = true
+        
     }
     
     override func viewDidAppear(_ animated: Bool) {
         navigationController?.navigationBar.isHidden = true
+        
     }
 
     @IBAction func enterButtonPressed(_ sender: UIButton) {
+       
     }
     
 }
